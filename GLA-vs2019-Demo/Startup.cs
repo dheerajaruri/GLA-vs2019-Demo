cac132931fc16562bc750708acf3b5469031df35
dheerajaruri <68398787+dheerajaruri@users.noreply.github.com>
2021-11-16 12:27:11 +0530

    Update Startup.cs
    added h1 tag

--- GLA-vs2019-Demo/Startup.cs
+++ GLA-vs2019-Demo/Startup.cs
@@ -32,7 +32,7 @@ namespace GLA_vs2019_Demo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("<h1>Hello World!</h1>");
                 });
             });
         }
